55058fa5611d7ee01db94b87a42513944ed218ba
antobinary <anto.georgiev@gmail.com>
2014-03-04 18:03:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
 var Hapi = require('hapi');
 var server = Hapi.createServer('0.0.0.0', parseInt(process.env.PORT, 10) || 4000);
-
-
-
-//Go to  192.168.0.231:4000/ and you will see "i am a beautiful butterfly"
-server.route({
-  method: 'GET'
-, path: '/'
-, handler: function(req, reply) {
-    reply('i am a beautiful butterfly');
-  }
-});
-
+console.log ("check");
 server.start();
-
-//Go to  192.168.0.231:4000/aa and you will see "AAAAAAAAAAAAA"
-server.route({
-  method: 'GET'
-, path: '/aa'
-, handler: function(req, reply) {
-    reply('AAAAAAAAAAA');
-  }
-});
-
 
 //Go to  192.168.0.231:4000/bigbluebutton/api and you will see "BBBBBBB"
 server.route({
@@ -35,10 +14,33 @@ server.route({
 
 
 var Joi = require('joi');
+
+
+
+
+
+
 var schema = {
     a: Joi.string()
 };
 
 
 var err = Joi.validate({ a: true }, schema);
+
+
+server.route({
+  method: 'GET'
+, path: '/bigbluebutton/api/create/{anton}'
+, handler: function(req, reply) {
+console.log (req.params.anton);
+    reply('BBBBBBBBBBB'+ req.params.anton);
+	
+  }
+});
+
+//http://192.168.0.231:4000/bigbluebutton/api/create/anton=ge     
+//BBBBBBBBBBBanton=ge
+
+
+
 
