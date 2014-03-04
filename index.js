e9cb899163f888577ac2dff61ee108720e326af2
antobinary <anto.georgiev@gmail.com>
2014-03-04 18:02:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var Hapi = require('hapi');
 var server = Hapi.createServer('0.0.0.0', parseInt(process.env.PORT, 10) || 4000);
 
+
+
+//Go to  192.168.0.231:4000/ and you will see "i am a beautiful butterfly"
 server.route({
   method: 'GET'
 , path: '/'
@@ -10,3 +13,32 @@ server.route({
 });
 
 server.start();
+
+//Go to  192.168.0.231:4000/aa and you will see "AAAAAAAAAAAAA"
+server.route({
+  method: 'GET'
+, path: '/aa'
+, handler: function(req, reply) {
+    reply('AAAAAAAAAAA');
+  }
+});
+
+
+//Go to  192.168.0.231:4000/bigbluebutton/api and you will see "BBBBBBB"
+server.route({
+  method: 'GET'
+, path: '/bigbluebutton/api'
+, handler: function(req, reply) {
+    reply('BBBBBBBBBBB' + err);
+  }
+});
+
+
+var Joi = require('joi');
+var schema = {
+    a: Joi.string()
+};
+
+
+var err = Joi.validate({ a: true }, schema);
+
